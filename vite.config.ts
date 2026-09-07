4a5e11c45e34d3d9ea8f190273ccb4c775db480e
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2026-09-07 22:50:04 +0200

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: '/decibel-meter/',
+  server: {
+    port: 5174,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    chunkSizeWarningLimit: 1000,
+  },
+})
